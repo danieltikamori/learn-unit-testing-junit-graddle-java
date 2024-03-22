942118ccbcb1a91ff4606253ca3f2e2a4711c02d
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-03-22 21:16:56 +0900

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-engine
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
 
 tasks.test {
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+
+// This class contains test cases for the Person class
 class PersonTest {
-  
+
+    // This test case checks the getBirthDate() method of the Person class
+    @Test
+    void getBirthDate() {
+
+    }
+
+    // This test case checks the getAge() method of the Person class
+    @Test
+    void getAge() {
+
+    }
+
+    // This test case checks the age calculation logic of the Person class
+    @Test
+    void validateAgeCalculation() {
+        // Create a new instance of the Person class with a birthdate of January 1, 2000
+        Person person = new Person("John", LocalDate.of(2000, 1, 1));
+        // Assert that the calculated age of the person is 24
+        Assertions.assertEquals(24, person.getAge());
+    }
 }
